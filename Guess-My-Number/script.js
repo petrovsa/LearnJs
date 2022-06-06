c40564886e249923b27d1eb8a59761e7fa370fd5
petrovsa <petrovsa@ukr.net>
2022-06-06 16:47:07 +0300

    Refactoring of the guessing game

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,39 +1,44 @@
 "use strict";
 
-// console.log(document.querySelector(".message").textContent);
-// document.querySelector(".message").textContent = "Correct number";
-// document.querySelector(".number").textContent = 20;
-// document.querySelector(".score").textContent = 20;
-// document.querySelector(".guess").value = 2;
 const number = Math.trunc(Math.random() * 20) + 1;
+const showMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   let inputField = Number(document.querySelector(".guess").value);
 
   if (!inputField) {
-    document.querySelector(".message").textContent = "Input value";
+    showMessage("Input value");
   } else if (inputField === number) {
-    document.querySelector(".message").textContent = "Correct number";
-    document.querySelector("body").style.backgroundColor = "#60b347";
+    showMessage("Correct number");
+
     document.querySelector(".number").textContent = number;
-  } else if (inputField > number) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too high";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "It is your lost game";
-      document.querySelector(".score").textContent = 0;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
     }
-  } else if (inputField < number) {
+  } else if (inputField !== number) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too low";
+      showMessage(inputField > number ? "Too high" : "Too low");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "It is your lost game";
+      showMessage("It is your lost game");
+
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").textContent = "?";
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  showMessage("Start guessing...");
+  document.querySelector("body").style.backgroundColor = "#222";
 });
